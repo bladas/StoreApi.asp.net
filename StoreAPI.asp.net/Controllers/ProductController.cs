6e2067b342faad0f0a8016a79d9edb1110486d4e
bladas <dashkevich_v@ukr.net>
2020-04-21 22:48:06 +0300

    add viewmodels and product controler

--- StoreAPI.asp.net/Controllers/ProductController.cs
+++ StoreAPI.asp.net/Controllers/ProductController.cs
@@ -4,12 +4,89 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Store.DAL.EF;
+using Store.DAL.Entities;
+using StoreAPI.asp.net.ViewModel;
 
 namespace StoreAPI.asp.net.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
+        private readonly AppDBContext _context;
+        public ProductController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("addproduct")]
+        public IActionResult AddProduct(ProductViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var product = new Product
+            {
+                Name = model.Name,
+                
+                Price = model.Price
+            };
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPut("updateproduct")]
+        public IActionResult Update(ProductViewModel model)
+        {
+            var product = _context.Products.FirstOrDefault(x => x.Id == model.Id);
+            if (product == null) return BadRequest();
+
+            product.Name = model.Name;
+            product.ShortDescription = model.ShortDescription;
+            product.Price = model.Price;
+
+            _context.Products.Update(product);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete("deleteproduct")]
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Products.FirstOrDefault(x => x.Id == id);
+            if (product == null) return BadRequest();
+
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+
+        [HttpGet("getproduct")]
+        public IActionResult GetProduct(int id)
+        {
+            var product = _context.Products.FirstOrDefault(x => x.Id == id);
+            if (product == null) return BadRequest();
+
+            return Ok(product);
+        }
+
+        [HttpGet("getallproducts")]
+        public IActionResult GetAllProducts()
+        {
+            var product = _context.Products.ToList();
+            if (product == null) return BadRequest();
+
+            return Ok(product);
+        }
+
     }
 }
--- StoreAPI.asp.net/ViewModel/ProductViewModel.cs
+++ StoreAPI.asp.net/ViewModel/ProductViewModel.cs
@@ -1,20 +1,32 @@
-﻿using System;
+﻿using Store.DAL.Entities;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Store.DAL.Entities
+namespace StoreAPI.asp.net.ViewModel
 {
-    public class Product
+    public class ProductViewModel
     {
+
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+        
         public string ShortDescription { get; set; }
-        public string LongDescription { get; set; }
-        public string Image { get; set; }
+        [Required]
         public decimal Price { get; set; }
+        
+        /*
+        public string LongDescription { get; set; }
+        
+
         public bool IsFavourite { get; set; }
         public int Available { get; set; }
         public int? CategoryId { get; set; }
         public virtual Category Category { get; set; }
+        */
     }
 }
